a3a996b183444e4c78ea229406865b74515a73a6
Gai Takakura <gixa.aturakka@gmail.com>
2020-04-22 23:36:31 +0900

    PushNotificationを追加

---
+++ PushNotification/PushNotification.tsx
@@ -0,0 +1,78 @@
+//--------------------------------------------------
+/**
+ * Expo Push Notifications Wraper
+ */
+//--------------------------------------------------
+/* React */
+import React from 'react';
+/* Expo */
+import * as Permissions from "expo-permissions";
+import { Notifications } from 'expo';
+
+/**
+ * Notification Base Data
+ * T: Object 
+ */
+export interface Notification<T = {}> {
+  origin: "selected"|"received";
+  remote: boolean;
+  data: T;
+}
+
+/**
+ * Wraper Component
+ * T: Object
+ */
+export class PushNotification<T = {}> extends React.Component<{
+  getToken?: (token: string) => void;
+  foreground?: (notification: Notification<T>) => void;
+  background?: (notification: Notification<T>) => void;
+}>{
+  private _notificationSubscription: any|undefined;
+
+  constructor(props: any){
+    super(props);
+  }
+
+  private handleNotification = ( notification: Notification<T> ): void => {
+    switch( notification.origin ){
+      case "received":  // Foreground Process
+        this.props.foreground && this.props.foreground(notification);
+        break;
+
+      case "selected":  // Background Process
+        this.props.background && this.props.background(notification);
+        break;
+    }
+  }
+  
+  public async componentDidMount(){
+    try {
+      // Check Permission
+      const getResult = await Permissions.getAsync( Permissions.NOTIFICATIONS );
+      if( getResult.status !== "granted" ){
+        const askResult = await Permissions.askAsync( Permissions.NOTIFICATIONS );
+        if( askResult.status !== "granted" ){
+          return;
+        }
+      }
+
+      // Get device token
+      const token = await Notifications.getExpoPushTokenAsync();
+      this.props.getToken && this.props.getToken( token );
+
+      // Set handle
+      this._notificationSubscription = Notifications.addListener( this.handleNotification );
+    } catch (error) {
+      console.log( error );
+    }
+  }
+
+  public componentWillUnmount(){
+    this._notificationSubscription && this._notificationSubscription.remove();
+  }
+
+  render(){
+    return this.props.children;
+  }
+}
---
+++ PushNotification/README.md
@@ -0,0 +1,53 @@
+# PushNotification
+プッシュ通知処理のラッパーコンポーネント。  
+デバイストークンの取得、ハンドルと登録・破棄を自動的に行う。  
+
+```ts
+import React from 'react';
+import { PushNotification } from './PushNotification';
+
+/*
+ * プッシュ通知で送られてくるオブジェクト
+ */
+interface NotificationObject{
+  title: string;
+  id: number;
+  uri?: string;
+}
+
+export default class App React.Component {
+
+  private getToken = (token: string) => {
+    console.log("Token :", token);
+  }
+
+  private foreground = ( notification: any ): void => {
+    console.log("Foreground :", notification.data);
+  }
+
+  private background = ( notification: any ): void => {
+    console.log("Background :", notification.data);
+  }
+
+  render(){
+    return(
+      <PushNotification<NotificationObject>
+        getToken={ this.getToken }
+        foreground={ this.foreground }
+        background={ this.background } >
+        <View/>
+      </PushNotification>
+    );
+  }
+}
+```
+
+## Props
+* getToken( token )  
+デバイストークン取得時の処理。起動時にパーミッションが許容されていれば走る。
+
+* foreground( notification )  
+アプリを実行している時に走る処理。  
+
+* background( notification )  
+アプリ未起動、またはバックグラウンドで実行している時に通知をタップした際に走る処理。
